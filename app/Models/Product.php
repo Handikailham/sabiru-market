3e6b88443e771cedd09c2ac2935325c092d4bd30
ilham <handikailhampratama@gmail.com>
2025-03-19 12:15:05 +0700

    Commit

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Product extends Model
 {
@@ -19,5 +20,27 @@ class Product extends Model
 {
     return $this->belongsTo(Category::class, 'id_kategori');
 }
+
+protected static function boot()
+    {
+        parent::boot();
+
+        // Hapus file saat record dihapus
+        static::deleting(function ($product) {
+            if ($product->gambar) {
+                Storage::disk('public_uploads')->delete($product->gambar);
+            }
+        });
+
+        // Hapus file lama saat gambar diupdate
+        static::updating(function ($product) {
+            if ($product->isDirty('gambar')) {
+                $oldImage = $product->getOriginal('gambar');
+                if ($oldImage) {
+                    Storage::disk('public_uploads')->delete($oldImage);
+                }
+            }
+        });
+    }
 
 }
